c5dabd640728e057ca6e4549c4e988e2b089574c
Bruno Paulin <brunopaulin@bpaulin.net>
2022-05-27 13:31:42 +0200

    ansible vscode ext

--- home-manager/ops/ops.nix
+++ home-manager/ops/ops.nix
@@ -6,11 +6,17 @@
     vault
   ];
 
-
   programs = {
     vscode = {
       extensions = with pkgs.vscode-extensions; [
         hashicorp.terraform
+      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "ansible-vault-inline";
+          publisher = "wolfmah";
+          version = "0.4.0";
+          sha256 = "sha256-pU2tKnYKZ/w02fD3BonrYByB+bsxx4ycqDO3q5ltzt4=";
+        }
       ];
     };
   };
