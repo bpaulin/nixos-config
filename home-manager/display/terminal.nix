fc47fe7d23235c1041fe0cf793987b0c8697d994
Bruno Paulin <brunopaulin@bpaulin.net>
2022-06-04 13:47:05 +0200

    guake key

--- home-manager/display/terminal.nix
+++ home-manager/display/terminal.nix
@@ -24,6 +24,11 @@
   };
 
   dconf.settings = {
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+      ];
+    };
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
       binding = "<Control>twosuperior";
       command = "guake-toggle";
