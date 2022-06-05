dcc84a145121052d0e1c8e991cdd6dac84f2907e
Bruno Paulin <brunopaulin@bpaulin.net>
2022-06-06 00:08:18 +0200

    hpc4

---
+++ machines/hpc4/configuration.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+
+{
+  networking.hostName = "hpc4";
+  imports = [
+    ./../../flavors/home/main.nix
+    ./../../flavors/x/main.nix
+    ./../../flavors/work/main.nix
+    ./../../flavors/devops/main.nix
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.initrd.luks.devices = {
+    root = {
+      name = "root";
+      device = "/dev/disk/by-uuid/dac31eae-6a2f-4b60-a4b9-625bb1e79fb0";
+      preLVM = true;
+      allowDiscards = true;
+    };
+  };
+}
+
