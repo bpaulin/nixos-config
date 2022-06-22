96b89f28c3d69d3bc90dbe45c6118cf376c78255
Bruno Paulin <brunopaulin@bpaulin.net>
2022-06-22 20:32:47 +0200

    lithium is coming

---
+++ machines/lithium/configuration.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }:
+
+{
+  networking.hostName = "lithium";
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # boot.initrd.luks.devices = {
+  #   root = {
+  #     name = "root";
+  #     device = "/dev/disk/by-uuid/dac31eae-6a2f-4b60-a4b9-625bb1e79fb0";
+  #     preLVM = true;
+  #     allowDiscards = true;
+  #   };
+  # };
+}
+
