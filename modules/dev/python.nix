5eae123a8cfa7787fa5c7b66ae1f2fbeb63021c0
Bruno Paulin <brunopaulin@bpaulin.net>
2022-08-05 08:49:52 +0200

    ansible-builder, ugly

--- modules/dev/python.nix
+++ modules/dev/python.nix
@@ -13,6 +13,45 @@ let
     };
   };
 
+  bindep = pkgs.python39Packages.buildPythonPackage rec {
+    pname = "bindep";
+    version = "2.11.0";
+    doCheck = false;
+
+    src = pkgs.python39Packages.fetchPypi {
+      inherit version;
+      pname = "bindep";
+      sha256 = "sha256-rLLyWbzh/RUIhzR5YJu95bmq5Qg3hHamjWtqGQAufi8=";
+    };
+    propagatedBuildInputs = [
+      python39Packages.pbr
+      python39Packages.distro
+      python39Packages.packaging
+    ];
+    postPatch = ''
+      # don't test bash builtins
+      rm requirements.txt
+    '';
+  };
+
+  ansible-builder = pkgs.python39Packages.buildPythonPackage rec {
+    pname = "ansible-builder";
+    version = "1.1.0";
+    doCheck = false;
+
+    src = pkgs.python39Packages.fetchPypi {
+      inherit version;
+      pname = "ansible-builder";
+      sha256 = "sha256-RVzWj7SyjHr5b9AM8jv5PM4HGu2u17tfToycSwn5qyA=";
+    };
+    propagatedBuildInputs = [
+      python39Packages.pbr
+      python39Packages.pyyaml
+      python39Packages.requirements-parser
+      bindep
+    ];
+  };
+
   molecule = pkgs.python39Packages.buildPythonPackage rec {
     pname = "molecule";
     version = "3.6.1";
@@ -52,6 +91,9 @@ let
       molecule
       scrapy
       aiohttp
+      cryptography
+      bindep
+      ansible-builder
     ];
   python-with-my-packages = python3.withPackages my-python-packages;
 in
