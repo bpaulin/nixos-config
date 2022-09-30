bf3a6227cc60b069d450421e9c4848ad088d8438
Bruno Paulin <brunopaulin@bpaulin.net>
2022-09-30 09:27:21 +0200

    doc user passwd

--- modules/system/user.nix
+++ modules/system/user.nix
@@ -7,6 +7,7 @@
     uid = 1000;
     extraGroups = [ "wheel" ];
     shell = pkgs.zsh;
+    # mkpasswd -m sha-512 // https://nixos.org/manual/nixos/stable/#sec-user-management
     hashedPassword = "$6$MDaoHcbuSiJQR83L$unAvaWD6YFLlimJSuKjYBCX8YBN3TWffMHelQBSv0uIZhC/4ISzcnkyKVu.9bbYX4SmTqUt37mQhYFXxpny23/";
   };
 
