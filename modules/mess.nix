85ebb2a6181eba04afe556de524ad8b773cd0a85
Bruno Paulin <brunopaulin@bpaulin.net>
2022-09-30 19:34:12 +0200

    structure mess

--- modules/mess.nix
+++ modules/mess.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }:
 {
+  environment.systemPackages = with pkgs; [
+    # packages
+  ];
+
   home-manager.users.bpaulin = { pkgs, ... }: {
     home.packages = with pkgs; [
       # packages
