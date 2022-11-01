f795a681569ee9bf5997245c4054c4f5823588e5
Bruno Paulin <brunopaulin@bpaulin.net>
2022-11-01 10:57:12 +0100

    init boron

--- machines/boron/configuration.nix
+++ machines/boron/configuration.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   networking.hostName = "boron";
@@ -10,18 +10,31 @@
     <nixos-hardware/common/pc/laptop>
     <nixos-hardware/common/cpu/intel>
     <nixos-hardware/common/gpu/intel.nix>
+    <nixos-hardware/common/gpu/nvidia.nix>
+    <nixos-hardware/common/gpu/nvidia-disable.nix>
   ];
+
+  services.thermald.enable = true;
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
-  boot.initrd.luks.devices = {
-    root = {
-      name = "root";
-      device = "/dev/disk/by-uuid/2f526857-4621-4e3d-a4cd-4268eda455b4";
-      preLVM = true;
-      allowDiscards = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  home-manager.users.bpaulin = { ... }: {
+    id_pub = {
+      bpaulin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIcmR6S6hpKRX8mTrNtfexDOAmO1FTb93Hxo6ne3clXB bpaulin@boron ";
+      oneup = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBIpyRf1jY2f4fWQvJFYC98MX/LwbSWu8zBs+0iwEPQN oneup@boron";
     };
+  };
+  hardware.nvidia.prime = {
+    # offload.enable = lib.mkDefault true;
+    # Hardware should specify the bus ID for intel/nvidia devicesi
+    # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
+    intelBusId = "PCI:0:2:0";
+
+    # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
+    nvidiaBusId = "PCI:1:0:0";
   };
 }
 
