dd29d3eb09c10b49f7b7c5125a65f860458a1b05
Bruno Paulin <brunopaulin@bpaulin.net>
2022-11-09 16:39:52 +0100

    nvidia power

--- machines/boron/configuration.nix
+++ machines/boron/configuration.nix
@@ -36,14 +36,20 @@
       oneup = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINLZ5gmGeAlOC/NZQ9sSD8kvZTcOqkE79c2t2xL20VTI oneup@boron";
     };
   };
-  hardware.nvidia.prime = {
-    # offload.enable = lib.mkDefault true;
-    # Hardware should specify the bus ID for intel/nvidia devicesi
-    # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
-    intelBusId = "PCI:0:2:0";
+  hardware.nvidia = {
+    prime = {
+      # offload.enable = lib.mkDefault true;
+      # Hardware should specify the bus ID for intel/nvidia devicesi
+      # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
+      intelBusId = "PCI:0:2:0";
 
-    # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
-    nvidiaBusId = "PCI:1:0:0";
+      # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
+      nvidiaBusId = "PCI:1:0:0";
+    };
+    powerManagement = {
+      enable = true;
+      finegrained = true;
+    };
   };
 }
 
