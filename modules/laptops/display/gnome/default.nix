2f33e13dfde612aaf0b2d2ea0dd2fd812fe1e83d
Bruno Paulin <brunopaulin@bpaulin.net>
2022-11-23 08:44:41 +0100

    no need to gnome keyring patch anymore

--- modules/laptops/display/gnome/default.nix
+++ modules/laptops/display/gnome/default.nix
@@ -1,10 +1,5 @@
 { ... }:
 {
-  # until https://github.com/NixOS/nixpkgs/pull/201229/commits/808095530a618c70e81c9f2423273860cfbed4d0 is on master
-  nixpkgs.overlays = [
-    (import ../../../../pkgs/overlays/gnome-keyring.nix)
-  ];
-
   services.xserver.desktopManager.gnome.enable = true;
 
   imports = [
@@ -34,6 +29,8 @@
       dconf2nix
       # to change settings manually
       gnome.gnome-tweaks
+      # mic test
+      gnome.gnome-sound-recorder
     ];
 
     # Gtk theme
