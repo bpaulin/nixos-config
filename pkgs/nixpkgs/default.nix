f6d79ebdc9a8fc8ae70fa5d43f7fbb3cb4ae6266
Bruno Paulin <brunopaulin@bpaulin.net>
2022-12-23 14:10:48 +0100

    last

--- pkgs/nixpkgs/default.nix
+++ pkgs/nixpkgs/default.nix
@@ -3,7 +3,6 @@ _self: super:
   # merged on nixpkgs \o/
   # pinniped = super.callPackage ./pinniped { };
   # vault-medusa = super.callPackage ./vault-medusa { };
-  infracost = super.callPackage ./infracost { };
   molecule = super.callPackage ./molecule { };
   ansible-builder = super.callPackage ./ansible-builder { };
   polaris = super.callPackage ./polaris { };
